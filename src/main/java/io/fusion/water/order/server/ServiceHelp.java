cf84289afd3c36c76953d8472594e50ebb1526fb
Araf Karsh Hamid <araf.karsh@gmail.com>
2021-08-01 09:16:10 +0530

    Order Service optimized

--- src/main/java/io/fusion/water/order/server/ServiceHelp.java
+++ src/main/java/io/fusion/water/order/server/ServiceHelp.java
@@ -54,7 +54,7 @@ public class ServiceHelp {
 	
 	public static final String DL = "----------------------------------------------------------------------------";
 	
-	public static final String VERSION = "0.7.0";
+	public static final String VERSION = "0.8.0";
 	
 	public static final String LOGO = "" +NL
 	+"============================================================================" + NL
